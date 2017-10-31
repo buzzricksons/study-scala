9eca9ec97f49049f003359f629f31c16df107721
Hyungcheol Kim <hcgoon+test@gmail.com>
2017-10-31 18:57:31 +0900

    edit main method

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -3,11 +3,6 @@ object MyModule {
     if (n < 0) -n
     else n
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
@@ -16,8 +11,27 @@ object MyModule {
     go(n, 1)
   }
 
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  def formatResult(name: String, n: Int, f:Int => Int) = {
+    val msg = "The %s fo %d is %d."
+    msg.format(name, n, f(n))
+  }
 
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+//    println(formatAbs(-42))
+//    println(formatFactorial(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+  }
 
 }

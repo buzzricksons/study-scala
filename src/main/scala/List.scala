6011e60182b40931605b465943bd0aa3c131b1ff
HyungCheol Kim <hcgoon@gmail.com>
2017-11-10 17:35:17 +0900

    edit List

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,4 +1,4 @@
-
+import MyModule01_02.findFirst
 
 sealed trait List[+A]
 case object Nil extends List[Nothing]
@@ -20,5 +20,11 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+
+  def main(args: Array[String]): Unit = {
+    val ex1: List[Double] = Nil
+    val ex2: List[Int] = Cons(1, Nil)
+    val ex3: List[String] = Cons("a", Cons("b", Nil))
+  }
 
 }

cd5845a8a94e52fdc18986f259cd221fbbc33f6e
HyungCheol Kim <hcgoon@gmail.com>
2017-11-28 19:40:42 +0900

    edit binary tree

--- src/main/scala/BinaryTree.scala
+++ src/main/scala/BinaryTree.scala
@@ -1,5 +1,3 @@
-import sun.reflect.generics.tree.Tree
-
 sealed trait BinaryTree {
   case class Leaf[A](value: A) extends Tree[A]
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]

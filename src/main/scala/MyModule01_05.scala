c88d6cd6e5f8cd7515edbf9e3947e352668889f0
HyungCheol Kim <hcgoon@gmail.com>
2018-01-17 18:04:58 +0900

    add exception example

--- src/main/scala/MyModule01_05.scala
+++ src/main/scala/MyModule01_05.scala
@@ -8,8 +8,35 @@ object MyModule01_05 {
     catch  { case e: Exception => 43}
   }
 
+  def failingFn2(i: Int): Int = {
+    try {
+      val x = 42 + 5
+      x + ((throw new Exception("fail!")): Int)
+    }
+    catch  { case e: Exception => 43}
+  }
+
+  def mean(xs: Seq[Double]): Double =
+    if (xs.isEmpty)
+      throw new ArithmeticException("mean of empty list!")
+    else xs.sum / xs.length
+
+  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double =
+    if (xs.isEmpty) onEmpty
+    else xs.sum / xs.length
+
+  sealed trait Option[+A]
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
+
+  def mean_option(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+
   def main(args: Array[String]): Unit = {
-    println(failingFn(12))
+//    println(failingFn2(12))
+//    println(failingFn(12))
   }
 
 }

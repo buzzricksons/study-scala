9ebc5f80975df020bfcfe18ae2d20f214d1bd5f7
HyungCheol Kim <hcgoon@gmail.com>
2018-01-18 16:17:05 +0900

    add exception example

--- src/main/scala/MyModule01_05.scala
+++ src/main/scala/MyModule01_05.scala
@@ -1,42 +1,10 @@
-object MyModule01_05 {
-  def failingFn(i: Int): Int = {
-    val y: Int = throw new Exception("fail!")
-    try {
-      val x = 42 + 5
-      x + y
-    }
-    catch  { case e: Exception => 43}
-  }
-
-  def failingFn2(i: Int): Int = {
-    try {
-      val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int)
-    }
-    catch  { case e: Exception => 43}
-  }
-
-  def mean(xs: Seq[Double]): Double =
-    if (xs.isEmpty)
-      throw new ArithmeticException("mean of empty list!")
-    else xs.sum / xs.length
-
-  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double =
-    if (xs.isEmpty) onEmpty
-    else xs.sum / xs.length
-
-  sealed trait Option[+A]
-  case class Some[+A](get: A) extends Option[A]
-  case object None extends Option[Nothing]
-
-  def mean_option(xs: Seq[Double]): Option[Double] =
-    if (xs.isEmpty) None
-    else Some(xs.sum / xs.length)
-
-
-  def main(args: Array[String]): Unit = {
-//    println(failingFn2(12))
-//    println(failingFn(12))
+object MyModule01_06 {
+  trait Option[+A] {
+    def map[B](f: A => B): Option[B]
+    def flatMap[B](f: A => Option[B]): Option[B]
+    def getOrElse[B >: A](default: => B): B
+    def orElse[B >: A](ob: => Option[B]): Option[B]
+    def filter(f: A => Boolean): Option[A]
   }
 
 }
